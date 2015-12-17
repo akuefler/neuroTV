c9864f9c922e25d75d3a5112b2a1a1b9278ba3c5
Alex Kuefler <kuefleralex@gmail.com>
2015-12-17 11:19:01 -0800

    Added epsilon-greedy setting for agent.act. Implemented Monte Carlo simulations for learning MDP P_sa and rewards.

--- reinforcement.py
+++ reinforcement.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+import copy
+
 NUM_TILES = 4
 NUM_ACTIONS = 4
 
@@ -24,6 +26,18 @@ def int2act(integ):
 
     return action
 
+def act2int(act):
+    if act == 'up':
+        i = 0
+    elif act == 'down':
+        i = 1
+    elif act == 'left':
+        i = 2
+    elif act == 'right':
+        i = 3
+
+    return i
+
 class Agent(object):
     """
     Abstract base class
@@ -41,12 +55,17 @@ class RandomAgent(Agent):
         action = legal[random.randint(0, len(legal)-1)]
         return action
 
+
 class PIagent(Agent):
     def __init__(self, mdp):
         self.V = np.zeros(NUM_TILES*NUM_TILES)
         self.mdp = mdp
 
-        self.pi = np.random.randint(0, NUM_ACTIONS, NUM_TILES**2)
+        #self.pi = np.random.randint(0, NUM_ACTIONS, NUM_TILES**2)
+        self.policy = np.ones(NUM_TILES**2)*np.nan
+        for s in range(0, NUM_TILES**2):
+            legal = self.mdp.getLegalActions(ind2sub(s), returnInts= True)
+            self.policy[s] = np.random.choice(legal)
 
         #Policy Iteration assumes these are known
         self.Psa = self.mdp.Psa
@@ -56,15 +75,14 @@ class PIagent(Agent):
         V= self.V.copy()
 
         for i in range(0,k):
-            V= [self.R[s] + self.mdp.discount * np.dot(self.Psa[self.pi[s],s,:], V) \
+            V= [self.R[s] + self.mdp.discount * np.dot(self.Psa[self.policy[s],s,:], V) \
              for s in range(0, NUM_TILES**2)]
 
         return V
 
-
     def policyIteration(self, TOL= 0.01):
         #c= 0
-        oldPI = self.pi.copy()
+        oldPI = self.policy.copy()
         while True:
             #Set values:
             self.V = self.policyEvaluation()
@@ -82,20 +100,23 @@ class PIagent(Agent):
                         a = idx
                         break
 
-                self.pi[s] = a
+                self.policy[s] = a
 
-            diff = np.linalg.norm(self.pi - oldPI)
+            diff = np.linalg.norm(self.policy - oldPI)
             print(diff)
             if diff < TOL:
                 #break
                 break
 
-            oldPI = self.pi.copy()
+            oldPI = self.policy.copy()
 
-    def act(self, state):
-        integ = self.pi[sub2ind(state.pos)]
-        return int2act(integ)
-
+    def act(self, state, epsilon = 0):
+        if np.random.rand() > epsilon:
+            a = self.policy[sub2ind(state.pos)]
+        else:
+            legal= self.mdp.getLegalActions(state.pos, returnInts = True)
+            a = np.random.choice(legal)
+        return int2act(a)
 
 
 class VIagent(Agent):
@@ -154,6 +175,50 @@ class VIagent(Agent):
 
         return action
 
+class MCagent(PIagent):
+    """
+    Monte Carlo reinforcement learning.
+
+    Subclass of PI agent.
+
+    ISSUE: It really just needs "act" from PIagent. Make this method of the superclass agent.
+    """
+    #def __init__(self, mdp):
+        #self.V
+        #self.mdp = mdp
+
+        #self.pi = np.random.randint(0, NUM_ACTIONS, NUM_TILES**2)
+
+    def estimate(self, k= 50):
+
+        C = np.zeros((NUM_ACTIONS, NUM_TILES**2, NUM_TILES**2))
+        R = np.zeros(NUM_TILES**2)
+
+        for i in range(0, k):
+            ##ISSUE: Can I do this with an mdp method? e.g., "simulate"
+            mdp = copy.deepcopy(self.mdp)
+            currState = mdp.state
+            while not currState.isEnd():
+                s = copy.deepcopy(currState)
+                action = self.act(s, epsilon = 0.2)
+                #print(action)
+                mdp.updateState(action)
+                currState = mdp.state
+
+                C[act2int(action), sub2ind(s.pos), sub2ind(currState.pos)] += 1
+                R[sub2ind(currState.pos)] = currState.getReward() #Assume deterministic reward.
+
+            Psa = np.zeros(C.shape)
+            #Normalize counts to obtain transition matrix.
+            for a in range(0,Psa.shape[0]):
+                for s in range(0,Psa.shape[1]):
+                    if sum(C[a,s,:])>0:
+                        Psa[a,s,:] = C[a,s,:]/sum(C[a,s,:])
+            self.R = R
+
+        pass
+
+
 class MDP(object):
     def __init__(self, startPos, goalTiles, failTiles, slipTiles, discount = 0.95):
         self.goalTiles = goalTiles
@@ -164,7 +229,7 @@ class MDP(object):
         self.reward = 0 #Deprecated?
         self.state = State(self, startPos)
 
-        self.Psa = self.createT()
+        self.Psa = self.createPsa()
         self.R = self.createR()
 
     def createR(self):
@@ -186,7 +251,7 @@ class MDP(object):
         return R
 
 
-    def createT(self):
+    def createPsa(self):
         """
         Creates the true transition probability matrix for the MDP.
         """
@@ -237,7 +302,8 @@ class MDP(object):
         return successor
 
     def getLegalActions(self, pos, returnInts = False):
-        ##ISSUE: Doesn't make sense to keep this part of "State".
+        ##ISSUE: Because "self" is never used, maybe this should be its own function entirely.
+
         actions = []
         actints = []
 
@@ -260,6 +326,8 @@ class MDP(object):
         return out
 
     def updateState(self, action):
+        assert self.state.isEnd() == False
+
         newPos = self.generateSuccessor(self.state.pos, action)
         self.state = State(self, newPos)
         self.reward += self.state.getReward()
@@ -295,17 +363,17 @@ vi.valueIteration()
 pi = PIagent(mdp)
 pi.policyIteration()
 
+mc = MCagent(mdp)
+mc.estimate(k = 1000)
+mc.policyIteration()
+
 currState = mdp.state
 while not currState.isEnd():
     #action= ra.act(currState)
-    action= pi.act(currState)
+    action= mc.act(currState)
     print(action)
     mdp.updateState(action)
     print(mdp.reward)
     currState = mdp.state
     print(currState.pos)
-
-
-
-
 
